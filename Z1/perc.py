6827813d7df7eae5d92bebb67b6e7193207a484c
Drejzer <drejzer@gmail.com>
2020-10-08 01:35:34 +0200

    first attempt at gui

--- Z1/perc.py
+++ Z1/perc.py
@@ -1,15 +1,39 @@
+import sys
 import pygame
 from pygame.locals import *
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((300,300))
-pygame.draw.circle(DISPLAYSURF, (0,0,0), (200,50),30)
+EKRAN = pygame.display.set_mode((900,900))
+pygame.display.update()
+
+slots = []
+for i in range(9):
+    row = []
+    for j in range(5):
+        row.append(False)
+    slots.append(row)
 
 while True:
-    pygame.display.update()
+    mpos=pygame.mouse.get_pos()
+    for i in range(9):
+        for j in range(5):
+            if ((100*j) <= mpos[0] < (100*(j+1))) and ((100*i) <= mpos[1] < (100*(i+1))):
+                pygame.draw.rect(EKRAN,(0,0,255),(100*j,100*i,100,100))
+    for i in range(9):
+        for j in range(5):
+            if slots[i][j]:
+                pygame.draw.rect(EKRAN,(255,255,255),(100*j,100*i,100,100))
+
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if event.type==QUIT:
             pygame.quit()
-            sye.exit()
-
-
+            sys.exit()
+        if event.type==MOUSEBUTTONUP:
+            slots[min(int(mpos[1]/100),8)][min(int(mpos[0]/100),4)]^=True
+    pygame.display.update()
+    for i in range(9):
+        for j in range(5):
+            if slots[i][j]:
+                pygame.draw.rect(EKRAN,(255,255,255),(100*j,100*i,100,100))
+            else:
+                pygame.draw.rect(EKRAN,(0,0,0),(100*j,100*i,100,100))
